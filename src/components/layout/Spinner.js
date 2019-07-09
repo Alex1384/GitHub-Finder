c5cd70965abc3a82ea8c104a1ec07f2c592626d6
PEYRAT ALEXANDRE <APE18558@open-groupe.com>
2019-07-09 14:28:58 +0200

    repo, repoItem done

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,8 +1,9 @@
-import React, {Fragment} from 'react'
+import React, {Fragment} from 'react';
+import spinner from './spinner.gif';
 
 
-const Spinner = () =>  <Fragment>
-      <img src="{{spinner}}" alt="Loading...." style={{width: '200px', margin: 'auto', display:'block'}}/>
+const Spinner = () => <Fragment>
+      <img src={spinner} alt="Loading...." style={{width: '200px', margin: 'auto', display:'block'}}/>
     </Fragment>
  
-export default Spinner
+export default Spinner;
